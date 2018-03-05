e27be730177f9efa763461089371baec9ea89727
JHealy <healyj5@tcd.ie>
2018-03-05 22:08:15 +0000

    Update exercise-5-iris-data.py

--- exercise-5-iris-data/exercise-5-iris-data.py
+++ exercise-5-iris-data/exercise-5-iris-data.py
@@ -9,5 +9,5 @@ with open('data/iris.data.csv', 'r') as f:
   for line in f:
     row = line.split(',')[0:4]
     row = list(map(float, row))
-    print('{:.1f} {:.1f} {:.1f} {:.1f}'.format(row[0],row[1],row[2],row[3]))
+    print('{:.1f} {:.1f} {:.1f} {:.1f}'.format(row[2],row[3],row[0],row[1]))
 
