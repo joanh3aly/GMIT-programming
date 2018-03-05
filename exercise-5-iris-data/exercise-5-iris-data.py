e796d410855e361650efd8c6a5f39fb3b20d5a35
JHealy <healyj5@tcd.ie>
2018-03-05 17:58:25 +0000

    Add files via upload

---
+++ exercise-5-iris-data/exercise-5-iris-data.py
@@ -0,0 +1,14 @@
+'''
+Joan Healy - Exercise 5
+Parse CSV data
+5/3/18
+'''
+
+
+with open('data/iris.data.csv', 'r') as f:
+  for index, line in enumerate(f):
+    row = line.split(',')[0:4]
+    row = list(map(float, row))
+    print('{:.1f} {:.1f} {:.1f} {:.1f}'.format(row[0],row[1],row[2],row[3]))
+
+
