f1f3a009282f738cecdd4a6663bc77f8d5f6bbfa
JHealy <healyj5@tcd.ie>
2018-04-03 10:53:16 +0100

    Update exercise-6.py

--- exercise-6.py
+++ exercise-6.py
@@ -4,14 +4,20 @@ Joan Healy
 5/3/2018
 '''
 
+# Create factorial function
 def factorial(x):
+  # Initialise counter to 1
   counter = 1
+  # Condition whereby if the function's argument is greater than 1 run code
   if x > 0:
+    # Loop from 1 to 1 plus the function's argument 'x', (as we want to start from 1 not the default 0)
     for each in range(1,x+1):
+      # multiply each iteration of the loop (all of the positive numbers less than x) by the counter and update counter to the result
       counter = each * counter
+  # return the result of counter
   return counter
-    
+
+# Print the factorial function of 5, 7 and 10 by calling the factorial function with each of these numbers as arguments   
 print(factorial(5))
 print(factorial(7))
 print(factorial(10))
-
